60baab93be69449d279224ee414595dd452dc47f
FerraroLucas <ferrarolucasmartin@gmail.com>
2022-09-29 20:42:42 -0300

    ejercicios 2, tipos

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+let nombre = "Lucas Ferraro";
+let edad = 28;
+let soydesarrollador = false;
+let nacimiento = new Date(2022 , 0 , 31);
+let libro = {
+    titulo:"Inteligencia Planetaria",
+    autor:"Eugenio Carutti",
+    fecha: new Date(2015 , 3),
+    url: "https://www.amazon.com/-/es/Eugenio-Carutti/dp/1503231593",
+
+}
+
+const Listaej2 = [nombre , edad , soydesarrollador , nacimiento , libro]
+console.log(Listaej2)
